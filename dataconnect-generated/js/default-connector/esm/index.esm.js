a48bc53851180dc35fcba01ae5cfe94bb7dba933
Hemant Kadam <99681551+hemantkadam112@users.noreply.github.com>
2024-12-05 12:15:05 +0530

    intial commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'kreateai',
+  location: 'asia-east1'
+};
+
